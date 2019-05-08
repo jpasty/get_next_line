82c02238f4aed1ea661de68074313a7df5659992
jpasty <nebalyi93@mail.ru>
2019-05-08 23:59:57 +0300

    New project file

---
+++ get_next_line.c
@@ -0,0 +1,9 @@
+#include "get_next_line.h"
+
+int				get_next_line(const int fd, char **line)
+{
+	static char **line;
+	char		*buf[BUF_SIZE + 1];
+	int			ret;
+	
+}
---
+++ get_next_line.h
@@ -0,0 +1,8 @@
+#ifendf GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+int		get_next_line(static int, char **);
+
+#endif
