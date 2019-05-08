ea6bf1abc397023da7533752f34f5b89fcd46784
jpasty <nebalyi93@gmail.com>
2019-05-09 01:16:05 +0300

    GNL in progress

---
+++ get_next_line.c
@@ -0,0 +1,33 @@
+#include "get_next_line.h"
+
+int				ft_next_line(char **line, const int fd, char **hold, int ret)
+{
+
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char *hold[FD_MAX];
+	char		buf[BUF_SIZE + 1];
+	char		*tmp;
+	int			ret;
+
+	if (fd < 0 || !line)
+		return (-1);
+	while (ret = read(fd, buf, BUFF_SIZE) > 0)
+	{
+		buf[ret] = '\0';
+		if (!hold[fd])
+			hold[fd] = ft_strnew(1);
+		tmp = ft_strjoin(hold[fd], buf);
+		free(hold[fd]);
+		hold[fd] = tmp;
+		if (ft_strchr(buf, '\n'))
+			break;
+	}
+	if (ret < 0)
+		return (-1);
+	if (ret == 0 && (hold[fd] == NULL || line[fd][0] == '\0'))
+		return (0);
+	return (ft_next_line(line, fd, hold, ret))
+}
---
+++ get_next_line.h
@@ -0,0 +1,12 @@
+#ifendf GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 1024
+# define FD_MAX
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(static int, char **);
+
+#endif
