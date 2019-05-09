199c0e946defe8249ee16fe68ad26d47a53d9080
jpasty <49813695+jpasty@users.noreply.github.com>
2019-05-09 14:25:44 +0300

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,10 +1,20 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpasty <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/09 12:49:14 by jpasty            #+#    #+#             */
+/*   Updated: 2019/05/09 14:20:10 by jpasty           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int				ft_next_line(char **line, const int fd, char **hold, int ret)
+int				ft_next_line(char **hold)
 {
-
+	
 }
 
 int				get_next_line(const int fd, char **line)
@@ -14,38 +24,20 @@ int				get_next_line(const int fd, char **line)
 	char		*tmp;
 	int			ret;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !line || (read(fd, NULL, 0) < 0))
 		return (-1);
 	while (ret = read(fd, buf, BUFF_SIZE) > 0)
 	{
 		buf[ret] = '\0';
 		if (!hold[fd])
-			hold[fd] = ft_strnew(1);
+			hold[fd] = ft_strnew(0);
 		tmp = ft_strjoin(hold[fd], buf);
 		free(hold[fd]);
 		hold[fd] = tmp;
 		if (ft_strchr(buf, '\n'))
 			break;
 	}
-	if (ret < 0)
-		return (-1);
-	if (ret == 0 && (hold[fd] == NULL || line[fd][0] == '\0'))
+	if (ret == 0 && (hold[fd] == NULL || hold[fd][0] == '\0'))
 		return (0);
-	return (ft_next_line(line, fd, hold, ret))
+	return (ft_next_line(&hold[fd], line, ret))
 }
-=======
-=======
->>>>>>> 82c02238f4aed1ea661de68074313a7df5659992
-#include "get_next_line.h"
-
-int				get_next_line(const int fd, char **line)
-{
-	static char **line;
-	char		*buf[BUF_SIZE + 1];
-	int			ret;
-	
-}
-<<<<<<< HEAD
->>>>>>> 82c02238f4aed1ea661de68074313a7df5659992
-=======
->>>>>>> 82c02238f4aed1ea661de68074313a7df5659992
